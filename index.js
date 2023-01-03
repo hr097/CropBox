f21de6844ebfbcfb1afc40b68f6800ec5b1c5eef
shubhamkhunt029 <shubhamkhunt029@gmail.com>
2023-01-03 20:22:29 +0530

    file input taken code has been altered

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const path = require("path");
 const logger = require("morgan");
 const cors = require("cors");
 
-app.use(upload());
+
 app.use(logger("dev"));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(upload());
 
 app.post('/',(req,res)=>{
   if(req.files)
