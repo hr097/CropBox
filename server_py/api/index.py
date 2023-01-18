509cdf97cc78c34ed8fa0d58901fb86d521c00af
hr097 <harshilramani9777@gmail.com>
2023-01-18 15:02:16 +0530

    Python test 1

--- server_py/api/index.py
+++ server_py/api/index.py
@@ -1,12 +1,21 @@
+# from http.server import BaseHTTPRequestHandler
 from sanic import Sanic
 from sanic.response import json
+
 app = Sanic()
+
+# class handler(BaseHTTPRequestHandler):
+
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type','application/json')
+#         self.end_headers()
+#         self.wfile.write('Hello, world!'.encode('utf-8'))
+#         return
 
 
 @app.route('/')
-async def index(request, path=""):
-    return json({'default': path})
-
 @app.route('/testing')
 async def index(request, path=""):
     return json({'hello': path})
+
